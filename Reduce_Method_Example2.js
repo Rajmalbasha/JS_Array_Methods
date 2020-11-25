df1f1d846ff586ed59cd1a0cd6ce64e1899dcf34
Rajmalbasha <rajmal046@gmail.com>
2020-11-25 17:23:26 +0530

    Filter and reduce combined

---
+++ Reduce_Method_Example2.js
@@ -0,0 +1,44 @@
+// Note : XMLHttpRequest is a built-in object in web browsers.
+// It is not distributed with Node; you have to install it separately.
+
+try 
+{
+    "use strict" // Avoids hoisting of variables
+
+    if ((typeof process !== 'undefined') && (process.release.name === 'node')) // Checks if it is running in node process and includes xmlhttprequest via a call to a function require
+    {
+        var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    }
+
+    var request = new XMLHttpRequest(); // creates a new xmlhttprequest object
+
+    request.open("GET", "https://restcountries.eu/rest/v2/all",true) // Configures the GET-request for the URL , does not open connection
+
+    request.send(); // Opens the connection and sends the request to server
+
+    // Called after receiving the response
+    request.onload = function()
+    {
+        
+        // Returns an array of objects and each object contains country name, region, sub region etc. all the details of the country
+        let data = JSON.parse(this.responseText)
+
+        // Returns an new array of objects whose region is Asia
+        const getAsianCountryObjects = data.filter((item) => {
+            return item.region === 'Asia'
+        })
+
+        // Executes provided function for each value of the array and reduce the array to single value. However it doesn't modify the original array
+        const totalPopulationOfAllAsianCountries = getAsianCountryObjects.reduce((result,item)=>{
+            return result+=item.population
+        },0)
+        
+        console.log("Total population of all the countries is :",totalPopulationOfAllAsianCountries)
+
+       
+    }
+    
+} 
+catch (error) {
+    console.error(error.name,error.message)
+}
